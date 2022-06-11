5bcfa7e93707629ef663e4aef306bf1660153718
doniaskima <doniaskima344@gmail.com>
2022-06-11 23:50:04 +0100

    change2

--- Clients & Servers/servers.js
+++ Clients & Servers/servers.js
@@ -10,12 +10,21 @@ const server = http.createServer((req, res) => {
     switch (req.url) {
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
+            break;
+            // redirects 
+        case '/about-us':
+            res.statusCode = 301;
+            res.setHeader('location', '/about');
+            res.end();
             break;
         default:
             falsepath += '404.html';
+            res.statusCode = 404;
             break;
     }
 
@@ -29,6 +38,9 @@ const server = http.createServer((req, res) => {
             // res.write(data);
             res.end(data);
         }
+        // res.write(data);
+        res.statusCode = 200;
+        res.end(data);
     })
 
 });
@@ -36,3 +48,13 @@ const server = http.createServer((req, res) => {
 server.listen(3000, 'localhost', () => {
     console.log('listenig for requuets on port 3000');
 });
+
+// 200 ok
+// 301 resource moved 
+//404 not found 
+//500 internal sever error
+
+
+
+
+// nodeman is a package which helps us to create a live reload server and tha's really gonna  speed up development process
